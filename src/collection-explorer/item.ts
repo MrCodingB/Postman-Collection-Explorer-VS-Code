e46e3f52badb574298e8d010bbe17016ca36767a
MrCodingB <bastiwoess@gmail.com>
2021-01-09 22:17:59 +0100

    add simple tree view

---
+++ src/collection-explorer/item.ts
@@ -0,0 +1,18 @@
+import { TreeItem, TreeItemCollapsibleState } from 'vscode';
+
+export class Item extends TreeItem {
+  constructor(
+    public readonly label: string,
+    private version: string,
+    public readonly collapsibleState: TreeItemCollapsibleState
+  ) {
+    super(label, collapsibleState);
+    this.tooltip = `${this.label}-${this.version}`;
+    this.description = this.version;
+  }
+
+  iconPath = {
+    light: '',
+    dark: ''
+  };
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ItemsProvider } from './collection-explorer/itemsProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,7 +19,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Postman-Collection-Explorer!');
-	});
+  });
+
+  vscode.window.registerTreeDataProvider('postmanCollectionExplorer', new ItemsProvider(vscode.workspace.rootPath ?? ''));
 
 	context.subscriptions.push(disposable);
 }
