b48ab1520f35accb899587a2c1c290c8b7c0a431
MrCodingB <bastiwoess@gmail.com>
2021-01-17 17:42:43 +0100

    fix parsing form newman summary to run summary

--- src/utils/runWithNewman.ts
+++ src/utils/runWithNewman.ts
@@ -1,11 +1,15 @@
-import { NewmanRunOptions, run } from 'newman';
+import { NewmanRunOptions, NewmanRunSummary, run } from 'newman';
 import { workspace } from 'vscode';
 import { RunSummary } from '../postman/newmanTypes';
+
+function toRunSummary(summary: NewmanRunSummary): RunSummary {
+  return JSON.parse(JSON.stringify(summary)) as RunSummary;
+}
 
 export async function runWithNewman(options: NewmanRunOptions): Promise<[Error | null, RunSummary]> {
   return new Promise<[Error | null, RunSummary]>((resolve) => {
     const workingDir = workspace.workspaceFolders ? workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-    run({ workingDir, ...options }, (err, summary) => resolve([err, summary]));
+    run({ workingDir, ...options }, (err, summary) => resolve([err, toRunSummary(summary)]));
   });
 }
