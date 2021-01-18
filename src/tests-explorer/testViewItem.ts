059af623e9c67dcddb2a2721465cb0a908a66689
MrCodingB <bastiwoess@gmail.com>
2021-01-18 20:50:11 +0100

    add type checks to tree view item

--- src/tests-explorer/testViewItem.ts
+++ src/tests-explorer/testViewItem.ts
@@ -6,6 +6,7 @@ export class TestViewItem extends TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: TreeItemCollapsibleState,
+    public readonly itemObject: RunSummary | RunExecution | RunExecutionAssertion,
     type: 'test' | 'request' | 'collection',
     public readonly status: 'passed' | 'failed' | 'errored' = 'errored',
     public readonly id?: string
@@ -20,7 +21,7 @@ export class TestViewItem extends TreeItem {
     const failed = summary.run.failures.length > 0;
     const status = summary.error ? 'errored' : failed ? 'failed' : 'passed';
 
-    return new TestViewItem(summary.collection.info.name, collapsibleState, 'collection', status, summary.collection.id);
+    return new TestViewItem(summary.collection.info.name, collapsibleState, summary, 'collection', status, summary.collection.id);
   }
 
   public static createFromExecution(execution: RunExecution): TestViewItem {
@@ -28,13 +29,25 @@ export class TestViewItem extends TreeItem {
     const failed = execution.assertions && execution.assertions.find((e) => e.error !== undefined) !== undefined;
     const status = failed ? 'failed' : 'passed';
 
-    return new TestViewItem(execution.item.name, collapsibleState, 'request', status, execution.item.id);
+    return new TestViewItem(execution.item.name, collapsibleState, execution, 'request', status, execution.item.id);
   }
 
   public static createFromAssertion(assertion: RunExecutionAssertion): TestViewItem {
     const failed = assertion.error !== undefined;
     const status = failed ? 'failed' : 'passed';
 
-    return new TestViewItem(assertion.assertion, TreeItemCollapsibleState.None, 'request', status);
+    return new TestViewItem(assertion.assertion, TreeItemCollapsibleState.None, assertion, 'test', status);
+  }
+
+  public isCollection(): this is { type: 'collection'; objectItem: RunSummary } {
+    return this.contextValue === 'collection';
+  }
+
+  public isRequest(): this is { type: 'request'; objectItem: RunExecution } {
+    return this.contextValue === 'request';
+  }
+
+  public isAssertion(): this is { type: 'test'; objectItem: RunExecutionAssertion } {
+    return this.contextValue === 'test';
   }
 }
