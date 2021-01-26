15ba475bc232aa92efa0e13189124cdf737a7d22
MrCodingB <bastiwoess@gmail.com>
2021-01-26 16:53:23 +0100

    remove description from tree view item

--- src/collection-explorer/treeViewItem.ts
+++ src/collection-explorer/treeViewItem.ts
@@ -14,7 +14,6 @@ export class TreeViewItem extends TreeItem {
   ) {
     super(label, collapsibleState);
     this.tooltip = `${this.label}`;
-    this.description = itemObject.description?.toString();
     this.id = this.itemObject.id;
     this.contextValue = this.isCollection() ? 'collection' : this.isFolder() ? 'folder' : 'request';
     this.iconPath = this.getIcon();
