b8091bc79ea1137e03453f698cfb2b7ffe9e2248
MrCodingB <bastiwoess@gmail.com>
2021-01-26 11:11:56 +0100

    fix root item not updating with item

--- src/postman/PostmanItem.ts
+++ src/postman/PostmanItem.ts
@@ -1,13 +1,31 @@
 import { PostmanNativeElement } from '../utils';
 
 export abstract class PostmanItem<T extends PostmanNativeElement = PostmanNativeElement> {
-  public id: string;
-  public name: string;
-  public description: string;
+  private _id: string;
+  private _name: string;
+  private _description: string;
+
+  get id(): string { return this._id; }
+  set id(value: string) {
+    this._id = value;
+    this.rootItem.id = value;
+  }
+
+  get name(): string { return this._name; }
+  set name(value: string) {
+    this._name = value;
+    this.rootItem.name = value;
+  }
+
+  get description(): string { return this._description; }
+  set description(value: string) {
+    this._description = value;
+    this.rootItem.description = value;
+  }
 
   constructor(public rootItem: T) {
-    this.id = rootItem.id;
-    this.name = rootItem.name;
-    this.description = rootItem.description?.toString() ?? '';
+    this._id = rootItem.id;
+    this._name = rootItem.name;
+    this._description = rootItem.description?.toString() ?? '';
   }
 }
