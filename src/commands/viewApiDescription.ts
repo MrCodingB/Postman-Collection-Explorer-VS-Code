a4bb722e00dd21397ec18f8f134eb644f349c92b
MrCodingB <bastiwoess@gmail.com>
2021-01-26 17:57:59 +0100

    recurse over children

--- src/commands/viewApiDescription.ts
+++ src/commands/viewApiDescription.ts
@@ -1,6 +1,21 @@
 import { commands, extensions, workspace } from 'vscode';
 import { TreeViewItem } from '../collection-explorer/treeViewItem';
+import { Collection } from '../postman/Collection';
+import { Folder } from '../postman/Folder';
+import { Request } from '../postman/Request';
 import { getCollection } from '../utils';
+
+function getFullDescription(item: Collection | Folder | Request): string[] {
+  const description = [item.description];
+
+  if (Request.isRequest(item)) {
+    return description;
+  }
+
+  item.children.forEach((c) => description.push(...getFullDescription(c)));
+
+  return description.filter((d) => d.trim() !== '');
+}
 
 export async function viewApiDescription(item: TreeViewItem): Promise<void> {
   const markdownExtension = extensions.getExtension('vscode.markdown-language-features');
@@ -12,9 +27,7 @@ export async function viewApiDescription(item: TreeViewItem): Promise<void> {
 
   const collection = getCollection(item.itemObject);
 
-  const description = [collection.description];
-
-  collection.children.forEach((c) => description.push(c.description));
+  const description = getFullDescription(collection);
 
   const descriptionDocument = await workspace.openTextDocument({ content: description.join('\n\n'), language: 'markdown' });
 
