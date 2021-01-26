d64c42451040f9d63c1feb23904205d965baa365
MrCodingB <bastiwoess@gmail.com>
2021-01-26 22:37:12 +0100

    add scripts to postman item

--- src/postman/PostmanItem.ts
+++ src/postman/PostmanItem.ts
@@ -1,9 +1,12 @@
+import { Event } from 'postman-collection';
 import { PostmanNativeElement } from '../utils';
 
 export abstract class PostmanItem<T extends PostmanNativeElement = PostmanNativeElement> {
   private _id: string;
   private _name: string;
   private _description: string;
+  private _prerequest: string;
+  private _test: string;
 
   get id(): string { return this._id; }
   set id(value: string) {
@@ -23,9 +26,38 @@ export abstract class PostmanItem<T extends PostmanNativeElement = PostmanNative
     this.rootItem.description = value;
   }
 
+  get prerequest(): string { return this._prerequest; }
+  set prerequest(value: string) {
+    this.prerequest = value;
+    const event = this.getListener('prerequest');
+    if (event) {
+      event.script.exec = value.split('\n');
+    } else {
+      this.rootItem.events.add(new Event({ script: value, listen: 'prerequest' }));
+    }
+  }
+
+  get test(): string { return this._test; }
+  set test(value: string) {
+    this._test = value;
+    const event = this.getListener('test');
+    if (event) {
+      event.script.exec = value.split('\n');
+    } else {
+      this.rootItem.events.add(new Event({ script: value, listen: 'test' }));
+    }
+  }
+
   constructor(public rootItem: T) {
     this._id = rootItem.id;
     this._name = rootItem.name;
     this._description = rootItem.description?.toString() ?? '';
+    this._prerequest = this.getListener('prerequest')?.script.toSource() ?? '';
+    this._test = this.getListener('test')?.script.toSource() ?? '';
+  }
+
+  private getListener(type: 'test' | 'prerequest'): Event | undefined {
+    const listeners = this.rootItem.events.listenersOwn(type);
+    return listeners.length > 0 ? listeners[0] : undefined;
   }
 }
