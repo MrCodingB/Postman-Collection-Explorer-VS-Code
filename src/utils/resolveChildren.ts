385f6df9cade35f437aa507fbffbebf2abe32aae
MrCodingB <bastiwoess@gmail.com>
2021-01-26 18:03:21 +0100

    add sorting of children

--- src/utils/resolveChildren.ts
+++ src/utils/resolveChildren.ts
@@ -7,5 +7,6 @@ import { isItemGroup } from './typeChecks';
 export function resolveChildren(itemGroup: ItemGroup<Item> | PmCollection, parent: Collection | Folder): (Folder | Request)[] {
   return itemGroup.items
     .all()
-    .map<Folder | Request>((i) => isItemGroup(i) ? new Folder(parent, i) : new Request(parent, i));
+    .map<Folder | Request>((i) => isItemGroup(i) ? new Folder(parent, i) : new Request(parent, i))
+    .sort((a, b) => Request.isRequest(a) && Folder.isFolder(b) ? 1 : Folder.isFolder(a) && Request.isRequest(b) ? -1 : 0);
 }
