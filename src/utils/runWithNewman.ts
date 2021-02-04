b8368e5a8703825557c2d7c30263d220783eba35
MrCodingB <bastiwoess@gmail.com>
2021-02-04 21:11:10 +0100

    use configuration when running newman

--- src/utils/runWithNewman.ts
+++ src/utils/runWithNewman.ts
@@ -1,5 +1,6 @@
 import { NewmanRunOptions, NewmanRunSummary, run } from 'newman';
 import { workspace } from 'vscode';
+import { EXTENSION_PREFIX } from '../commands/commands';
 import { RunSummary } from '../postman/newmanTypes';
 
 function toRunSummary(summary: NewmanRunSummary): RunSummary {
@@ -14,6 +15,13 @@ export async function runWithNewman(options: NewmanRunOptions): Promise<[Error |
   return new Promise<[Error | null, RunSummary]>((resolve) => {
     const workingDir = workspace.workspaceFolders ? workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-    run({ workingDir, insecure: true, ...options }, (err, summary) => resolve([err, toRunSummary(summary)]));
+    const config = workspace.getConfiguration(EXTENSION_PREFIX);
+    const strictSSL = config.get<boolean>('strictSSL', true);
+    const followRedirects = config.get<boolean>('followRedirects', true);
+
+    run(
+      { workingDir, insecure: !strictSSL, ignoreRedirects: !followRedirects, ...options },
+      (err, summary) => resolve([err, toRunSummary(summary)])
+    );
   });
 }
