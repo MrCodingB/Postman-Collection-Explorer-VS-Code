3d2c03d1fba140b71c44cc90dfb2c93c8c171ca0
MrCodingB <bastiwoess@gmail.com>
2021-02-06 20:00:40 +0100

    Prepare v0.2.0 (#7)

--- scripts/clean-build.js
+++ scripts/clean-build.js
@@ -4,8 +4,9 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const { join } = require('path');
 
-console.log('Removing previous build output');
-fs.rmSync(join(__dirname, '..', 'out'), { recursive: true, force: true })
+console.log('Removing previous build outputs');
+fs.rmSync(join(__dirname, '..', 'out'), { recursive: true, force: true });
+fs.rmSync(join(__dirname, '..', 'dist'), { recursive: true, force: true });
 
 console.log('Compiling');
 execSync('npm run compile');
